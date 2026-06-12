c0eab727d2ffb8c3d9b59aa0ff4cea08a37a6658
phorton1 <available@upon.request>
2026-06-12 16:43:03 +0000

    add navMateGUI build

---
+++ navMateGUI.pm
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+#------------------------------------------------------------------
+# a shell around navMate.pm
+#------------------------------------------------------------------
+# for separate Cava packaging of a GUI executable
+# which needs a separate script.
+
+use strict;
+use warnings;
+
+require "navMate.pm";
+	# run the program
+
+1;
